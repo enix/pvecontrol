c85e29e4e9ce30df199f5d5b503fde4b787e1060
Laurent Corbes <laurent.corbes@enix.fr>
2023-06-06 18:18:49 +0200

    Add some logging

--- src/pvecontrol.py
+++ src/pvecontrol.py
@@ -4,8 +4,8 @@ import os
 import sys
 import argparse
 import confuse
-import pprint
 import urllib3
+import logging
 from prettytable import PrettyTable
 from proxmoxer import ProxmoxAPI
 from collections import OrderedDict
@@ -73,6 +73,7 @@ def _parser():
   # Parser configuration
   parser = argparse.ArgumentParser(description='Proxmox VE control cli.')
   parser.add_argument('-v', '--verbose', action='store_true')
+  parser.add_argument('--debug', action='store_true')
   parser.add_argument('-c', '--cluster', action='store', required=True, help='Proxmox cluster name as defined in configuration' )
   subparsers = parser.add_subparsers(help='sub-command help', dest='action_name')
 
@@ -85,18 +86,20 @@ def _parser():
 
   return parser.parse_args()
 
+
 # Disable urllib3 warnings about invalid certs
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 args = _parser()
-# Start
-print("Proxmox cluster: %s" % args.cluster)
-# Load configuration file
+# configure logging
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG if args.debug else logging.INFO)
+logging.info("Proxmox cluster: %s" % args.cluster)
 
+# Load configuration file
 # FIXME Creation du directory de configuration si non existant
-#print('configuration directory is', config.config_dir())
+logging.debug('configuration directory is %s'%config.config_dir())
 validconfig = config.get(configtemplate)
-#pprint.pprint(validconfig)
+logging.debug('configuration is %s'%validconfig)
 
 # FIXME trouver une methode plus clean pour recuperer la configuration du bon cluster
 # Peut etre rework la configuration completement avec un dict
@@ -107,7 +110,7 @@ for c in validconfig.clusters:
 if not clusterconfig:
   print('No such cluster %s'%args.cluster)
   sys.exit(1)
-#pprint.pprint(clusterconfig)
+logging.debug('clusterconfig is %s'%clusterconfig)
 
 proxmox = ProxmoxAPI(clusterconfig.host, user=clusterconfig.user, password=clusterconfig.password, verify_ssl=False)
 
