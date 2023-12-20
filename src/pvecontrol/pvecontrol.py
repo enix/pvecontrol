206dc8322cf8fc7eb0bae3c3385fef4bd7f8a7ea
Laurent Corbes <laurent.corbes@enix.fr>
2023-12-20 17:30:03 +0100

    feat(pvecontrol): Add nodeevacuate feature
    This fonction allow to automatically migrate out all the VMs from a node to another ones.

--- src/pvecontrol/pvecontrol.py
+++ src/pvecontrol/pvecontrol.py
@@ -12,7 +12,9 @@ from collections import OrderedDict
 from humanize import naturalsize
 
 from node import PVENode
+from node import NodeStatus
 from vm import PVEVm
+from vm import VmStatus
 from task import PVETask
 from cluster import PVECluster
 
@@ -81,6 +83,101 @@ def action_nodelist(proxmox, args):
   """List proxmox nodes in the cluster using proxmoxer api"""
   nodes = [ _filter_keys(n.__dict__, ['node', 'status', 'allocatedcpu', 'maxcpu', 'mem', 'allocatedmem', 'maxmem']) for n in proxmox.nodes ]
   _print_tableoutput(nodes, sortby='node')
+
+def action_nodeevacuate(proxmox, args):
+  """Evacuate a node by migrating all it's VM out"""
+  # check node exists
+  srcnode = proxmox.find_node(args.node)
+  logging.debug(srcnode)
+  if not srcnode:
+    print("Node %s does not exist"%args.node)
+    return
+  # check node is online
+  if srcnode.status != NodeStatus.online:
+    print("Node %s is not online"%args.node)
+    return
+
+  targets = []
+  # compute targets migration possible
+  if args.target:
+    for t in list(set(args.target)):
+      if t == srcnode.node:
+        print("Target node %s is the same as source node, skipping"%t)
+        continue
+      tg = proxmox.find_node(t)
+      if not tg:
+        print("Target node %s does not exist, skipping"%t)
+        continue
+      if tg.status != NodeStatus.online:
+        print("Target node %s is not online, skipping"%t)
+        continue
+      targets.append(tg)
+  else:
+    targets = [n for n in proxmox.nodes if n.status == NodeStatus.online and n.node != srcnode.node]
+  if len(targets) == 0:
+    print("No target node available")
+    return
+  logging.debug("Migration targets: %s"%([t.node for t in targets]))
+
+  plan = []
+  for vm in srcnode.vms:
+    logging.debug("Selecting node for VM: %i, maxmem: %i, cpus: %i"%(vm.vmid, vm.maxmem, vm.cpus))
+    if vm.status != VmStatus.running and not args.no_skip_stopped:
+      logging.debug("VM %i is not running, skipping"%(vm.vmid))
+      continue
+    # check ressources
+    for target in targets: 
+      logging.debug("Test target: %s, allocatedmem: %i, allocatedcpu: %i"%(target.node, target.allocatedmem, target.allocatedcpu))
+      if (vm.maxmem + target.allocatedmem) > (target.maxmem - validconfig.node.memoryminimum):
+        logging.debug("Discard target: %s, will overcommit ram"%(target.node))
+      elif (vm.cpus + target.allocatedcpu) > (target.maxcpu *  validconfig.node.cpufactor):
+        logging.debug("Discard target: %s, will overcommit cpu"%(target.node))
+      else:
+        plan.append({"vmid": vm.vmid, "vm": vm, "node": args.node, "target": target})
+        target.allocatedmem += vm.maxmem
+        target.allocatedcpu += vm.cpus
+        logging.debug("Selected target %s: new allocatedmem %i, new allocatedcpu %i"%(target.node, target.allocatedmem, target.allocatedcpu))
+        break
+    else:
+      print("No target found for VM %s"%vm.vmid)
+      
+
+  logging.debug(plan)
+  # validate input
+  if len(plan) == 0:
+    print("No VM to migrate")
+    return
+  for p in plan:
+    print("Migrating VM %s (%s) from %s to %s"%(p['vmid'], p['vm'].name, p['node'], p['target'].node))
+  confirmation = input('Confirm (yes):')
+  logging.debug("Confirmation input: %s"%confirmation)
+  if confirmation.lower() != "yes":
+    print("Aborting")
+    return
+  # run migrations
+
+  for p in plan:
+    logging.debug("Migrating VM %s from %s to %s"%(p['vmid'], p['node'], p['target'].node))
+    print("Migrate VM: %i / %s from %s to %s"%(p['vmid'], p['vm'].name, p['node'], p['target'].node))
+    if not args.dry_run:
+      upid = p['vm'].migrate(p['target'].node, args.online)
+      logging.debug("Migration UPID: %s"%upid)
+      proxmox.refresh()
+      task = proxmox.find_task(upid)
+      if args.follow:
+        _print_task(proxmox, upid, args.follow)
+      else:
+        _print_taskstatus(task)
+      # wait for task completion
+      while task.running():
+        logging.debug("Task status: %s", task.runningstatus)
+        task.refresh()
+        time.sleep(1)
+      _print_taskstatus(task)
+
+    else:
+      print("Dry run, skipping migration")
+
 
 def action_vmlist(proxmox, args):
   """List VMs in the Proxmox Cluster"""
@@ -165,7 +262,7 @@ def _print_task(proxmox, upid, follow = False):
       time.sleep(1)
     _print_taskstatus(task)
   else:
-    _print_tableoutput([{"log output": Tasks.decode_log(log)}])
+    _print_tableoutput([{"log output": task.decode_log()}])
 
 def action_sanitycheck(proxmox, args):
   """Check status of proxmox Cluster"""
@@ -179,7 +276,7 @@ def action_sanitycheck(proxmox, args):
   # VM is started but 'startonboot' not set
   # VM is running in cpu = host
   # VM is running in cpu = qemu64
-  
+
 
 def _parser():
 ## FIXME
@@ -199,6 +296,16 @@ def _parser():
   # nodelist parser
   parser_nodelist = subparsers.add_parser('nodelist', help='List nodes in the cluster')
   parser_nodelist.set_defaults(func=action_nodelist)
+  # nodeevacuate parser
+  parser_nodeevacuate = subparsers.add_parser('nodeevacuate', help='Evacuate an host by migrating all VMs')
+  parser_nodeevacuate.add_argument('--node', action='store', required=True, help="Node to evacuate")
+  parser_nodeevacuate.add_argument('--target', action='append', required=False, help="Destination Proxmox VE node, you can specify multiple target options")
+  parser_nodeevacuate.add_argument('-f', '--follow', action='store_true', help="Follow task log output")
+  parser_nodeevacuate.add_argument('--online', action='store_true', help="Online migrate the VM, default True", default=True)
+  parser_nodeevacuate.add_argument('--no-skip-stopped', action='store_true', help="Don't skip VMs that are stopped")
+  parser_nodeevacuate.add_argument('--dry-run', action='store_true', help="Dry run, do not execute migration")
+  parser_nodeevacuate.set_defaults(func=action_nodeevacuate)
+
   # vmlist parser
   parser_vmlist = subparsers.add_parser('vmlist', help='List VMs in the cluster')
   parser_vmlist.set_defaults(func=action_vmlist)
@@ -223,6 +330,7 @@ def _parser():
   # sanitycheck parser
   parser_sanitycheck = subparsers.add_parser('sanitycheck', help='Run Sanity checks on the cluster')
   parser_sanitycheck.set_defaults(func=action_sanitycheck)
+
 
   # _test parser, hidden from help
   parser_test = subparsers.add_parser('_test')
