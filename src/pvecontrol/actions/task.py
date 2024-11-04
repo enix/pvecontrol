a79c2ec52abbb179db028bcddd3dfd1cd6122ea6
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2024-11-04 17:27:16 +0100

    fix: PEP8 compliance update private method

--- src/pvecontrol/actions/task.py
+++ src/pvecontrol/actions/task.py
@@ -1,9 +1,9 @@
-from pvecontrol.utils import _print_task, _print_tableoutput, _filter_keys
+from pvecontrol.utils import print_task, print_tableoutput, filter_keys
 
 
 def action_tasklist(proxmox, args):
-  tasks = [ _filter_keys(t.__dict__, ['upid', 'exitstatus', 'node', 'type', 'starttime', 'endtime', 'runningstatus', 'description']) for t in proxmox.tasks ]
-  _print_tableoutput(tasks, sortby='starttime')
+  tasks = [ filter_keys(t.__dict__, ['upid', 'exitstatus', 'node', 'type', 'starttime', 'endtime', 'runningstatus', 'description']) for t in proxmox.tasks ]
+  print_tableoutput(tasks, sortby='starttime')
 
 def action_taskget(proxmox, args):
-  _print_task(proxmox, args.upid, args.follow)
+  print_task(proxmox, args.upid, args.follow)
