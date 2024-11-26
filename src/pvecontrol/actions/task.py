cf68826fea54dfc3d61f695529a56c4224a15b17
Paul Laffitte <paul.laffitte@enix.fr>
2024-11-26 18:04:57 +0100

    feat: --columns flag

--- src/pvecontrol/actions/task.py
+++ src/pvecontrol/actions/task.py
@@ -1,9 +1,8 @@
 from pvecontrol.utils import print_task, print_tableoutput, filter_keys
-from pvecontrol.task import columns
 
 
 def action_tasklist(proxmox, args):
-  print_tableoutput(proxmox.tasks, columns, sortby=args.sort_by, filters=args.filter)
+  print_tableoutput(proxmox.tasks, columns=args.columns, sortby=args.sort_by, filters=args.filter)
 
 def action_taskget(proxmox, args):
   print_task(proxmox, args.upid, args.follow, args.wait, show_logs = True)
