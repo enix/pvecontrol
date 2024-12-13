91e07e5bad8161d363c35d12db949ace0a45e20c
Paul Laffitte <paul.laffitte@enix.fr>
2024-12-13 14:27:10 +0100

    tests: mock api, run tests in CI and add test_PVECluster_find_node

--- tests/test_pvecontrol.py
+++ tests/test_pvecontrol.py
@@ -1,7 +1,7 @@
 from pvecontrol.utils import filter_keys
 
 
-def testfilter_keys():
+def test_filter_keys():
     input_d = {"test": "toto", "none": "noninclude"}
     filter_k = ["test"]
     assert filter_keys(input_d, filter_k) == {"test": "toto"}
