f488dbe62a68042cfa0fb647a942152e50c41ae8
Paul Laffitte <paul.laffitte@enix.fr>
2024-12-20 12:54:51 +0100

    refactor: add PVEVolume and PVEBackupJob classes

--- src/pvecontrol/vm.py
+++ src/pvecontrol/vm.py
@@ -75,13 +75,13 @@ class PVEVm:
     def get_backup_jobs(self, proxmox):
         vm_backups = []
         for backup in proxmox.backup_jobs:
-            if str(self.vmid) in backup["vmid"].split(","):
+            if str(self.vmid) in backup.vmid.split(","):
                 vm_backups.append(backup)
         return vm_backups
 
     def get_backups(self, proxmox):
-        return [backup for backup in proxmox.backups if backup["vmid"] == self.vmid]
+        return [backup for backup in proxmox.backups if backup.vmid == self.vmid]
 
     def get_last_backup(self, proxmox):
-        backups = sorted(self.get_backups(proxmox), key=lambda x: x["ctime"])
+        backups = sorted(self.get_backups(proxmox), key=lambda x: x.ctime)
         return backups[-1] if len(backups) > 0 else None
