769bf68e528861bdd1e7b86b03dec460eb5d1a18
Paul Laffitte <paul.laffitte@enix.fr>
2024-12-20 11:05:19 +0100

    refactor: move backups and backups_job listing in PVECluster and PVEVm

--- src/pvecontrol/cluster.py
+++ src/pvecontrol/cluster.py
@@ -14,6 +14,8 @@ class PVECluster:
         self.api = ProxmoxAPI(host, user=user, password=password, timeout=timeout, verify_ssl=verify_ssl)
         self.name = name
         self.config = config
+        self._backups = None
+        self._backup_jobs = None
         self._initstatus()
 
     def _initstatus(self):
@@ -154,3 +156,17 @@ class PVECluster:
             "memory": self.memory_metrics(),
             "disk": self.disk_metrics(),
         }
+
+    @property
+    def backups(self):
+        if self._backups is None:
+            self._backups = []
+            for item in PVEStorage.get_grouped_list(self):
+                self._backups.extend(item["storage"].get_content("backup"))
+        return self._backups
+
+    @property
+    def backup_jobs(self):
+        if self._backup_jobs is None:
+            self._backup_jobs = self.api.cluster.backup.get()
+        return self._backup_jobs
