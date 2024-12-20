8403837e6f083c9a0ed8d7e08334782f0b86c79c
Paul Laffitte <paul.laffitte@enix.fr>
2024-12-20 17:38:15 +0100

    refactor: remove dead code in Check

--- src/pvecontrol/sanitycheck/checks.py
+++ src/pvecontrol/sanitycheck/checks.py
@@ -74,7 +74,6 @@ class Check(ABC):
             messages = []
         self.proxmox = proxmox
         self.messages = messages
-        self.code = ""
 
     @abstractmethod
     def run(self):
@@ -103,9 +102,6 @@ class Check(ABC):
             self.messages.append(messages)
         elif isinstance(messages, list):
             self.messages += messages
-
-    def set_code(self, code: CheckCode):
-        self.code = code
 
     def display(self, padding_max_size):
         if terminal_support_colors():
