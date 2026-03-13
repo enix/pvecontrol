fe41bb61723646f54aa1e6e8f39cbeac2c212a6f
Laurent Corbes <laurent.corbes@enix.fr>
2026-03-13 14:52:40 +0100

    fix(tests): remove unneded future

--- src/pvecontrol/sanitycheck/sanitychecks.py
+++ src/pvecontrol/sanitycheck/sanitychecks.py
@@ -9,9 +9,7 @@ from typing import TYPE_CHECKING
 >>>>>>> theirs
 from pvecontrol.sanitycheck.checks import CheckCode
 from pvecontrol.sanitycheck.tests import DEFAULT_CHECKS, DEFAULT_CHECK_IDS
-
-if TYPE_CHECKING:
-    from pvecontrol.models.cluster import PVECluster
+from pvecontrol.models.cluster import PVECluster
 from pvecontrol.models.cluster import PVECluster
 
 
