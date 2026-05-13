86e8938165d12e5a6a0aa2a2621626cd6d2b0208
Paul Laffitte <paul.laffitte@enix.fr>
2026-05-13 17:13:21 +0200

    tests: use PVEControlTestcase in report tests

--- src/tests/testcase.py
+++ src/tests/testcase.py
@@ -15,6 +15,8 @@ from tests.fixtures.api import (
 )
 from pvecontrol.models.cluster import PVECluster
 
+# class BasePVEControlTestcase(unittest.TestCase):
+
 
 class PVEControlTestcase(unittest.TestCase):
 
@@ -45,8 +47,24 @@ class PVEControlTestcase(unittest.TestCase):
         ]
         self.storages_contents = {node["status"]["name"]: {"s3": self.backups} for node in self.nodes}
 
+        self.ha_rules = None
+        self.ha_resources = None
+        self.users = None
+        self.groups = None
+        self.acls = None
+        self._extra_fixtures()
+
         self.responses_get = create_response_wrapper(
-            self.nodes, self.vms, self.backup_jobs, self.storage_resources, self.storages_contents
+            nodes=self.nodes,
+            vms=self.vms,
+            backup_jobs=self.backup_jobs,
+            storage_resources=self.storage_resources,
+            storages_contents=self.storages_contents,
+            ha_rules=self.ha_rules,
+            ha_resources=self.ha_resources,
+            users=self.users,
+            groups=self.groups,
+            acls=self.acls,
         )
 
         self.responses_get("/api2/json/version")
@@ -56,6 +74,7 @@ class PVEControlTestcase(unittest.TestCase):
             name = node["status"]["name"]
             self.responses_get(f"/api2/json/nodes/{name}/version")
             self.responses_get(f"/api2/json/nodes/{name}/qemu")
+        self._extra_routes()
 
         with patch("proxmoxer.backends.https.ProxmoxHTTPAuth") as mock_auth:
             mock_auth_instance = mock_auth.return_value
@@ -64,16 +83,47 @@ class PVEControlTestcase(unittest.TestCase):
             self.cluster = PVECluster(
                 "name",
                 "host",
-                config={
-                    "node": {
-                        "cpufactor": 2.5,
-                        "memoryminimum": 81928589934592,
-                    },
-                    "vm": {
-                        "max_last_backup": 100,
-                    },
-                },
+                config=self._cluster_config(),
                 verify_ssl=False,
                 **{"user": "user", "password": "password"},
                 timeout=mock_auth_instance.timeout,
             )
+        self._post_setup()
+
+    def _extra_fixtures(self):
+        """Subclass hook to set self.ha_rules / users / groups / acls before the response wrapper is built."""
+
+    def _extra_routes(self):
+        """Subclass hook to register additional response routes."""
+
+    def _post_setup(self):
+        """Subclass hook called once self.cluster is built (response mock still active)."""
+
+    def _cluster_config(self):
+        return {
+            "node": {
+                "cpufactor": 2.5,
+                "memoryminimum": 81928589934592,
+            },
+            "vm": {
+                "max_last_backup": 100,
+            },
+        }
+
+    def _register_full_routes(self):
+        for vm in self.vms:
+            self.responses_get(f"/api2/json/nodes/{vm['node']}/qemu/{vm['vmid']}/config")
+        for node in self.nodes:
+            name = node["status"]["name"]
+            for storage in self.storage_resources:
+                self.responses_get(
+                    f"/api2/json/nodes/{name}/storage/{storage['storage']}/content",
+                    params={"content": "backup"},
+                )
+        self.responses_get("/api2/json/cluster/ha/rules")
+        self.responses_get("/api2/json/cluster/ha/status/manager_status")
+        self.responses_get("/api2/json/cluster/ha/resources")
+        self.responses_get("/api2/json/cluster/backup")
+        self.responses_get("/api2/json/access/users")
+        self.responses_get("/api2/json/access/groups")
+        self.responses_get("/api2/json/access/acl")
